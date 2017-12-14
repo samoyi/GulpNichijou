0331eca82c6df7b7ac378ddc2cde761a2523bd42
samoyi <greatlining@163.com>
2017-12-14 20:01:59 +0800

    添加了arguments.js统一管理参数，使参数从gulpfile.js中分离。但addSuffix还不能递归子目录改名

---
+++ arguments.js
@@ -0,0 +1,17 @@
+
+exports.addSuffix = '123'; // addSuffix的后缀字符串
+
+// 可以通过 od-路径 打开的所有路径
+exports.od = {
+    'www': 'E:\\lining\\WWW',
+    'gits': 'E:\\lining\\WWW\\gits',
+    'test': 'E:\\lining\\WWW\\test',
+    'cs': 'E:\\lining\\WWW\\CS',
+    '2017': 'E:\\lining\\WWW\\Funca\\FuncaProjects\\2017',
+    'd': 'D:',
+};
+
+// 可以通过 of-文件名 打开的所有文件的路径
+exports.of = {
+    'ftp': 'E:\\lining\\WWW\\8UFTP.exe'
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,65 @@
 "use strict";
 
+const args = require('./arguments');
+
+
+// modules ---------------------------------------------------------------------
 const fs = require("fs"),
       path = require('path');
 
 const gulp = require('gulp');
+const rename = require('gulp-rename');
 
 const Win = require('./myModules/windows');
+const win = new Win();
 
 
-// 打开指定目录
-gulp.task('od-www', function(){
-    let win = new Win();
-    win.openDir('E:\\lining\\WWW');
-});
-gulp.task('od-gits', function(){
-    let win = new Win();
-    win.openDir('E:\\lining\\WWW\\gits');
-});
-gulp.task('od-test', function(){
-    let win = new Win();
-    win.openDir('E:\\lining\\WWW\\test');
-});
-gulp.task('od-cs', function(){
-    let win = new Win();
-    win.openDir('E:\\lining\\WWW\\CS');
-});
-gulp.task('od-2017', function(){
-    let win = new Win();
-    win.openDir('E:\\lining\\WWW\\Funca\\FuncaProjects\\2017');
-});
-gulp.task('od-d', function(){
-    let win = new Win();
-    win.openDir('D:');
-});
+
+// 打开指定目录 ------------------------------------------------------------------
+for(let dirName in args.od){
+    gulp.task('od-'+dirName, function(){
+        win.openDir(args.od[dirName]);
+    });
+}
 
 
-// 打开指定文件
-gulp.task('of-ftp', function(){
-    let win = new Win();
-    win.openFile('E:\\lining\\WWW\\8UFTP.exe');
+// 打开指定文件 ------------------------------------------------------------------
+for(let fileName in args.of){
+    gulp.task('of-'+fileName, function(){
+        win.openFile(args.of[fileName]);
+    });
+}
+
+
+// 文件操作 ---------------------------------------------------------------------
+
+// 从0开始对 indexRenamePic 文件夹中的文件以序号重命名
+// 使用时要保证indexRenamePic文件夹中只有待重命名的文件
+gulp.task('indexRenamePic', function(){
+    let index = 0;
+    gulp.src('tools/indexRenamePic/*.*')
+        .pipe(rename(function(path){
+            path.basename = index++;
+        }))
+        .pipe(gulp.dest('tools/indexRenamePic/renamed'));
 });
 
+// 获得 filenames 文件夹里所有文件的名字，生成 names.txt 文件中并写入获得的文件名
+gulp.task('filenames', function(){
+    let aNames = fs.readdirSync('tools/filenames/');
+    aNames.forEach((name)=>{
+        let basename = name.slice(0, name.indexOf('.'));
+        fs.appendFileSync('tools/filenames/names.txt', basename + '\r\n');
+    });
+});
 
-//
+// 文件名加后缀
+// 可以递归所有的子目录
+gulp.task('addSuffix', function(){
+    if(!args.addSuffix) return;
+    gulp.src('tools/addSuffix/*.*')
+        .pipe(rename(function(path){
+            path.basename += args.addSuffix;
+        }))
+        .pipe(gulp.dest('tools/addSuffix/withSuffix'));
+});
